76520808d38abc198ebe16118fe718bedd13907d
Afzal-Patel <aspb2017@mymail.pomona.edu>
2021-02-28 11:46:05 -0800

    initial commit

--- sql/07.sql
+++ sql/07.sql
@@ -6,3 +6,22 @@
  * This can be solved either with a LEFT JOIN or with the NOT IN operator.
  * You may choose whichever solution makes the most sense to you.
  */
+
+SELECT DISTINCT(title) 
+FROM film
+JOIN inventory USING (film_id)
+WHERE title NOT IN
+
+--move titles that have been rented & paid by customers w/ U.S. address
+    (SELECT DISTINCT(title)
+    FROM film
+    JOIN inventory USING (film_id)
+    JOIN rental USING (inventory_id)
+    JOIN payment USING (customer_id)
+    JOIN customer USING (customer_id)
+    JOIN address USING (address_id)
+    JOIN city USING (city_id)
+    JOIN country USING (country_id)
+    WHERE country='United States')
+ORDER BY title ASC;
+
